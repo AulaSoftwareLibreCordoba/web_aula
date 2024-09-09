bdac3301b9ee8e6f843a7ee0aab45a12777626ef
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-10 01:21:49 +0200

    cuarto intento de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify"
+import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify/edge-functions';
 import react from "@astrojs/react";
 
 // https://astro.build/config

58ac68ba7404e51af9747fb9e13fa0a5eb1df474
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-10 01:01:33 +0200

    segundo intento de despliegue en netlify

--- .netlify/v1/functions/ssr/.netlify/build/pages/index.astro.mjs
+++ .netlify/v1/functions/ssr/.netlify/build/pages/index.astro.mjs
@@ -1,5 +1,5 @@
 /* empty css                                 */
-import { c as createComponent, r as renderTemplate, m as maybeRenderHead, d as renderHead, a as addAttribute, e as renderComponent, f as renderSlot, b as createAstro } from '../chunks/astro/server_BkUrnYda.mjs';
+import { c as createComponent, r as renderTemplate, m as maybeRenderHead, a as addAttribute, d as renderHead, e as renderComponent, f as renderSlot, b as createAstro } from '../chunks/astro/server_BkUrnYda.mjs';
 import 'kleur/colors';
 import 'html-escaper';
 import 'clsx';
@@ -10,11 +10,11 @@ const $$Header = createComponent(($$result, $$props, $$slots) => {
 }, "C:/Users/alvar/web_aula/src/components/Header.astro", void 0);
 
 const $$Hero = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`<head>${renderHead()}</head> <section class="flex flex-col items-center justify-center pt-32 md:pt-24 lg:pt-28 xl:pt-32 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-[#22235f] via-[#27286b] to-[#0d0d30] bg-center bg-cover"> <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center"> <h1 class="max-w-lg mx-auto font-bold text-4xl md:text-5xl text-white mb-4 leading-tight"> <span id="animatedTitle" class="text-white "></span> <span id="animatedTitle2" class="text-blue-600"></span> <span id="cursor" class="text-white">|</span> </h1> <p class="max-w-md mx-auto text-sm font-normal text-gray-500 mb-6">
-Transformamos Ideas en Soluciones Abiertas, Fomentando el Conocimiento
-          y la Creatividad en Cada Proyecto.
+  return renderTemplate`${maybeRenderHead()}<section class="flex flex-col items-center justify-center pt-32 md:pt-24 lg:pt-28 xl:pt-32 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-[#22235f] via-[#27286b] to-[#0d0d30] bg-center bg-cover"> <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center"> <h1 class="max-w-lg mx-auto font-bold text-4xl md:text-5xl text-white mb-4 leading-tight"> <span id="animatedTitle" class="text-white"></span> <span id="animatedTitle2" class="text-blue-600"></span> <span id="cursor" class="text-white">|</span> </h1> <p class="max-w-md mx-auto text-sm font-normal text-gray-500 mb-6">
+Transformamos Ideas en Soluciones Abiertas, Fomentando el Conocimiento y
+      la Creatividad en Cada Proyecto.
 </p> <a class="cursor-pointer w-auto mb-10 inline-flex items-center justify-center py-3 px-6 text-base font-semibold text-center text-white rounded-full bg-blue-600 shadow-xs hover:bg-blue-800 transition-all duration-500">
-Únete a nosotros <svg class="ml-2" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.5 15L11.0858 11.4142C11.7525 10.7475 12.0858 10.4142 12.0858 10C12.0858 9.58579 11.7525 9.25245 11.0858 8.58579L7.5 5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg> </a> <div class="flex justify-center"> <img src="../../photo.webp" alt="Dashboard image" class="rounded-t-3xl h-auto object-cover w-[90%] md:w-[65%] shadow-2xl"> </div> </div> </section>`;
+Únete a nosotros <svg class="ml-2" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.5 15L11.0858 11.4142C11.7525 10.7475 12.0858 10.4142 12.0858 10C12.0858 9.58579 11.7525 9.25245 11.0858 8.58579L7.5 5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg> </a> <div class="flex justify-center"> <img src="../../photo.webp" alt="Dashboard image" class="rounded-t-3xl h-auto object-cover w-[90%] md:w-[65%] shadow-2xl"> </div> </div> </section> `;
 }, "C:/Users/alvar/web_aula/src/components/Hero.astro", void 0);
 
 const $$Astro = createAstro();
--- src/components/HeroJs.js
+++ src/components/HeroJs.js
@@ -1,42 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const titleElement1 = document.getElementById("animatedTitle");
-  const titleElement2 = document.getElementById("animatedTitle2");
-  const cursorElement = document.getElementById("cursor");
+    document.addEventListener("DOMContentLoaded", function () {
+    const titleElement1 = document.getElementById("animatedTitle");
+    const titleElement2 = document.getElementById("animatedTitle2");
+    const cursorElement = document.getElementById("cursor");
 
-  if (!titleElement1 || !titleElement2 || !cursorElement) {
-      console.error("Algunos elementos no se encontraron.");
-      return;
-  }
+    if (!titleElement1 || !titleElement2 || !cursorElement) {
+        console.error("Algunos elementos no se encontraron.");
+        return;
+    }
 
-  const texts = ["Bienvenidos al ", "Aula de Software Libre"];
-  let currentIndex = 0; // Controla qué texto se está escribiendo
-  let index = 0; // Controla la posición del carácter en el texto actual
+    const texts = ["Bienvenidos al ", "Aula de Software Libre"];
+    let currentIndex = 0; // Controla qué texto se está escribiendo
+    let index = 0; // Controla la posición del carácter en el texto actual
 
-  function type() {
-      // Determina el elemento actual y el texto a escribir
-      const currentElement = currentIndex === 0 ? titleElement1 : titleElement2;
-      const currentText = texts[currentIndex];
+    function type() {
+        // Determina el elemento actual y el texto a escribir
+        const currentElement = currentIndex === 0 ? titleElement1 : titleElement2;
+        const currentText = texts[currentIndex];
 
-      if (index < currentText.length) {
-          currentElement.innerHTML += currentText.charAt(index);
-          index++;
-          setTimeout(type, 100);
-      } else {
-          // Al terminar el primer texto, avanzar y esperar antes de comenzar el siguiente
-          if (currentIndex < texts.length - 1) {
-              currentIndex++;
-              index = 0;
-              setTimeout(type, 0); // Esperar un segundo antes de iniciar el siguiente texto
-          } else {
-              cursorElement.style.display = "none"; // Ocultar cursor al final
-          }
-      }
-  }
+        if (index < currentText.length) {
+        currentElement.innerHTML += currentText.charAt(index);
+        index++;
+        setTimeout(type, 100);
+        } else {
+        // Al terminar el primer texto, avanzar y esperar antes de comenzar el siguiente
+        if (currentIndex < texts.length - 1) {
+            currentIndex++;
+            index = 0;
+            setTimeout(type, 0); // Esperar un segundo antes de iniciar el siguiente texto
+        } else {
+            cursorElement.style.display = "none"; // Ocultar cursor al final
+        }
+        }
+    }
 
-  type(); // Iniciar la escritura
+    type(); // Iniciar la escritura
 
-  // Manejo del cursor
-  setInterval(() => {
-      cursorElement.style.visibility = cursorElement.style.visibility === "hidden" ? "visible" : "hidden";
-  }, 500);
-});
+    // Manejo del cursor
+    setInterval(() => {
+        cursorElement.style.visibility =
+        cursorElement.style.visibility === "hidden" ? "visible" : "hidden";
+    }, 500);
+    });

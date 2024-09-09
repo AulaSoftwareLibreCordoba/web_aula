62ba3c5fb65e87a11cf5727da51dc397c6fbc393
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-10 01:27:32 +0200

    intento en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
-import netlify from '@astrojs/netlify/edge-functions';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
   integrations: [tailwind(), react()]
 });

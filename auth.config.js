b59e9c6e008ba215f21f98cdb3fda2c599ee6f7f
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-11-27 15:26:11 +0100

    intento vuelta atras

--- auth.config.js
+++ auth.config.js
@@ -1,5 +1,3 @@
-//auth.config.js
-
 import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
 import { defineConfig } from 'auth-astro';
@@ -7,7 +5,7 @@ import { defineConfig } from 'auth-astro';
 export default defineConfig({
   providers: [
     GitHub({
-      clientId: import.meta.env.GITHUB_CLIENT_ID,
+      clientId: import.meta.env.GITHUB_CLIENT_ID, 
       clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
     }),
     Google({
